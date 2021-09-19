b511cf93c1e7a946970b98d1a40bc18c9abec302
Rafikooo <thecrazyrafik@gmail.com>
2021-09-19 15:18:53 +0200

    foundry

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,21 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Translation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
+        $translation = new Translation();
+        $translation->setSource("aaa");
         // $manager->persist($product);
+        $manager->persist($translation);
 
         $manager->flush();
     }
